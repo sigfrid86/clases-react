6734e276f1a982f472ea911742a16f72474db38a
Miguel Rivera <sigfrid86@hotmail.com>
2022-07-16 11:04:22 -0500

    ejercicio 2

--- src/App.js
+++ src/App.js
@@ -1,110 +1,35 @@
-import { useState, useEffect } from 'react';
-import { recetas } from './recetas'
 import styled from 'styled-components';
-import Card from './Card'
-import Imagen from './Imagen'
-import Title from './Title';
-import Type from './Type'
-import Cat from './Cat'
-import Download from './Download';
-import Counter from './Counter';
+import { useEffect, useState } from 'react';
 const App = () => {
-  const [filter, setFilter] = useState('')
-  const [recipes, setRecipes] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [data, setData] = useState([])
+  const [page, setPage] = useState(1)
   useEffect(()=>{
-    if(filter !== ''){
-      let recetasFiltradas = recetas.filter(receta => receta.tipo === filter)
-      setRecipes(recetasFiltradas)
-    }else{
-      setRecipes(recetas)
+    setIsLoading(true)
+    fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
+    .then(response => response.json())
+    .then(dataResponse => {
+      setIsLoading(false)
+      setData(dataResponse.results)
+    })
+  },[page])
+  return(<>
+    {isLoading &&
+      <h1>Cargando...</h1>
     }
-  },[filter])
-  return (<>
-    <Menu>
-      <button
-        className={filter === 'bebida' ? 'selected' : ''}
-        onClick={()=>setFilter('bebida')}>Bebida</button>
-      <button
-        className={filter === 'Desayuno' ? 'selected' : ''}
-        onClick={()=>setFilter('Desayuno')}>Desayuno</button>
-      <button 
-        className={filter === 'Sopa' ? 'selected' : ''}
-        onClick={()=>setFilter('Sopa')}>Sopa</button>
-      <button
-        className={filter === 'Ensalada' ? 'selected' : ''}
-        onClick={()=>setFilter('Ensalada')}>Ensalada</button>
-      <button
-        onClick={()=>setFilter('')}>X</button>
-    </Menu>
-    {filter !== ''  &&
-      <FilterTitle>Tu filtro es: {filter}</FilterTitle>
+    {!isLoading &&
+      <div>
+        <h1>Los personajes de la pagina {page}:</h1>
+        <ul>
+          {
+            data.map(character => (<li key={character.id}>{character.name}</li>))
+          }
+        </ul>
+        <button onClick={()=>setPage(page + 1)}>Siguiente</button>
+      </div>
     }
-   <Contenedor>
-     {
-        recipes.map(receta => {
-          return(
-            <Card key={receta.id}>
-              <Imagen 
-                url={receta.img_gde} 
-                tile={receta.title} 
-              />
-              <Title>{receta.title}</Title>
-              <Type>{receta.tipo}</Type>
-              <CatContainer>
-                {receta.categorias.map(categoria => {
-                  return(
-                    <Cat 
-                      key={categoria.id}
-                      url={categoria.icon} 
-                      title={categoria.title}
-                    />
-                  )
-                })}
-              </CatContainer>
-              <Download 
-                url={receta.pdf}
-              />
-              <Counter/>
-            </Card>
-          )
-        })
-     }
-   </Contenedor>
   </>);
 }
-const Contenedor = styled.section`
-  padding: 50px 5% ;
-  display: grid ;
-  grid-template-columns: repeat(4, 3fr) ;
-  gap:30px;
-`
-const CatContainer = styled.section`
-  display: flex ;
-  flex-wrap: wrap ;
-  gap:5px;
-  padding:5px ;
-`
-const Menu = styled.div`
-  display:flex ;
-  justify-content:center ;
-  gap:10px;
-  padding-top: 50px ;
-  button{
-    background-color: #FFF ;
-    padding:10px;
-    border: 1px solid black ;
-    border-radius:10px ;
-    cursor: pointer;
-    &.selected{
-      background-color: black ;
-      color:#FFF;
-    }
-  }
-`
-const FilterTitle = styled.div`
-  text-align:center;
-  margin-top:10px ;
-`
 
 
 export default App
--- src/base.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components"
-const XXX = props => {
-  return(<></>)
-}
-export default XXX
